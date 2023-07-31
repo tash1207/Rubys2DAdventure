028c0aca83f611e57a9858c2c7228cad02a630b2
Tasha Hankewych <thankewych@gmail.com>
2023-07-31 10:13:02 -0500

    Add two moving bots that do damage

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public int direction;
+    public float changeTime = 2.0f;
+
+    Rigidbody2D rigidbody2d;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+
+        if (vertical)
+        {
+            position.y = position.y + speed * direction * Time.deltaTime;
+        }
+        else
+        {
+            position.x = position.x + speed * direction * Time.deltaTime;
+        }
+        
+        rigidbody2d.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
